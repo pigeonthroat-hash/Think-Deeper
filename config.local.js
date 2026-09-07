24ee122354c2cd2a7ecf487f95330ea664790b93
pigeonthroat-hash <pigeonthroat@gmail.com>
2026-09-07 20:58:14 +1000

    Update config.local.js with Firebase settings
    Added Firebase configuration to local settings.

--- config.local.js
+++ config.local.js
@@ -3,5 +3,13 @@
  * Replace the value below with your Google OAuth Web Client ID.
  */
 window.__THINK_DEEPER_CONFIG = {
-  googleClientId: "1072960877495-j3eelm91rv1eailv16u98pqldnkdem9h.apps.googleusercontent.com"
+  googleClientId: "1072960877495-j3eelm91rv1eailv16u98pqldnkdem9h.apps.googleusercontent.com",
+  firebaseConfig: {
+    apiKey: "AIzaSyAKHoIoyHPwWolcyXW32abSwCD4Y8cHOUk",
+    authDomain: "think-deeper.firebaseapp.com",
+    projectId: "think-deeper",
+    storageBucket: "think-deeper.firebasestorage.app",
+    messagingSenderId: "74317161457",
+    appId: "1:74317161457:web:ea3773b5d3d773adf09a50"
+  }
 };
